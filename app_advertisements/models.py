b579e86d0f30eef6889661d207ffaa5bb8dc479b
nikitaa667766 <nikk2472@gmail.com>
2023-08-14 20:52:31 +0300

    ppp

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+
+User= get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
@@ -10,6 +15,11 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user= models.ForeignKey(User,verbose_name="пользователь",on_delete=models.CASCADE)
+    image=models.ImageField('картинка', upload_to='advertisements') 
+    
+    
+    
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -23,6 +33,12 @@ class Advertisement(models.Model):
             updated_time = self.updated_at.time().strftime("%H:%M:%S")
             return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time)
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+    
+    @admin.display(description='Картинка')
+    def photo (self):
+        if self.image:
+            return format_html('<img src="{url}" style="max-width:80px;max-height:80px>',url=self.image.url)
 
 
     def __str__(self) -> str:
